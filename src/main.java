c13057d979c47bacbeafe950f71c0824cba2fd32
Zamza584 <mzamalloa97@gmail.com>
2021-05-15 13:21:05 -0500

    added line of code

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ public class main {
 
     public static void main(String[] arg){
         System.out.println("Hello this is Ellen");
+        System.out.println("Hello Ellen, my name is Manuel");
 
     }
 }
